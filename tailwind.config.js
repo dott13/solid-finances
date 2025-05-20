946d6f87c943666d9326ee30aac9c3fdde2d16c7
dott13 <supreme.notfound@gmail.com>
2025-05-20 22:14:41 +0300

    Initial SolidJS + Tailwindcss setup

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ['./index.html', './src/**/*.{ts,tsx,js,jsx}'],
+    darkMode: 'class',
+    theme: { extend: {} },
+    plugins: [],
+  }
+  
